06b2df63a817277792710b062956bdd85803fb55
kamamerona <zszs2020@naver.com>
2023-07-19 21:27:46 +0900

    [feat] Content에 대한 데이터 형식 추가

---
+++ app/src/main/java/com/fast/blindappexample/data/model/entity/ContentEntity.kt
@@ -0,0 +1,28 @@
+package com.fast.blindappexample.data.model.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "Content")
+data class ContentEntity(
+    // 서버에 가지고 온 데이터로 지속적으로 업데이트를 해주기 때문에
+    // autoGenerate = false로 설정해준다.
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    @ColumnInfo
+    var title: String,
+    @ColumnInfo
+    var content: String,
+    @ColumnInfo
+    var category: String,
+    @ColumnInfo
+    val createdDate: Date,
+    @ColumnInfo
+    val likeCount: Int,
+    @ColumnInfo
+    val commentCount: Int,
+    @ColumnInfo
+    val viewCount: Int,
+) : java.io.Serializable
