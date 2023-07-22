b7e56ff61ffebba5fba54c74cf3f41a2589aaf3f
kamamerona <zszs2020@naver.com>
2023-07-22 14:01:04 +0900

    [fix] 데이터 포맷 변경

--- app/src/main/java/com/fast/blindappexample/util/DateUtil.kt
+++ app/src/main/java/com/fast/blindappexample/util/DateUtil.kt
@@ -5,9 +5,9 @@ import java.util.*
 
 object DateUtil {
 
-    val yyyy_MM_dd = SimpleDateFormat("yyyy-MM-dd")
+    val yyyy_MM_dd = SimpleDateFormat("yyyy.MM.dd")
     val dbDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-    val serverDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    val serverDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
 
     fun convertPrintDateString(date: Date? = Date()) = yyyy_MM_dd.format(date)
 }
