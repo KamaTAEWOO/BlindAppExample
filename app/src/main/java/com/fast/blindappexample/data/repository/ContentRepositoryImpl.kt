4807e5562ec48878fb1498ab947d6d2040fc81b1
kamamerona <zszs2020@naver.com>
2023-07-22 16:02:45 +0900

    [feat] update 기능 생성

--- app/src/main/java/com/fast/blindappexample/data/repository/ContentRepositoryImpl.kt
+++ app/src/main/java/com/fast/blindappexample/data/repository/ContentRepositoryImpl.kt
@@ -1,21 +1,45 @@
 package com.fast.blindappexample.data.repository
 
+import com.fast.blindappexample.data.model.ContentMapper.toContent
 import com.fast.blindappexample.data.model.ContentMapper.toEntity
 import com.fast.blindappexample.data.model.ContentMapper.toRequest
 import com.fast.blindappexample.data.source.local.dao.ContentDao
 import com.fast.blindappexample.data.source.remote.api.ContentService
 import com.fast.blindappexample.domain.model.Content
 import com.fast.blindappexample.domain.repository.ContentRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ContentRepositoryImpl @Inject constructor(
     private val contentService: ContentService,
     private val contentDao: ContentDao
 ) : ContentRepository {
+    override fun loadList(): Flow<List<Content>> {
+        return flow {
+            emit(
+                try {
+                    contentService.getList().data.map { it.toContent() }
+                } catch (e: Exception) {
+                    emptyList()
+                }
+            )
+        }
+    }
 
     override suspend fun save(item: Content): Boolean {
         return try {
             contentService.saveItem(item.toRequest())
+            contentDao.insert(item.toEntity())
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    override suspend fun update(item: Content): Boolean {
+        return try {
+            contentService.updateItem(item.toRequest())
             contentDao.insert(item.toEntity())
             true
         } catch (e: Exception) {
--- app/src/main/java/com/fast/blindappexample/domain/repository/ContentRepository.kt
+++ app/src/main/java/com/fast/blindappexample/domain/repository/ContentRepository.kt
@@ -1,8 +1,13 @@
 package com.fast.blindappexample.domain.repository
 
 import com.fast.blindappexample.domain.model.Content
+import kotlinx.coroutines.flow.Flow
 
 interface ContentRepository {
 
+    fun loadList() : Flow<List<Content>>
+
     suspend fun save(item: Content) : Boolean
+
+    suspend fun update(item: Content) : Boolean
 }
