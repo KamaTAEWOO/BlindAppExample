ce59739d3abb93e16eb6d90e29ccb4338bbd2781
kamamerona <zszs2020@naver.com>
2023-07-22 13:51:07 +0900

    [feat] Content 모델 생성

--- app/src/main/java/com/fast/blindappexample/data/model/entity/ContentEntity.kt
+++ app/src/main/java/com/fast/blindappexample/data/model/entity/ContentEntity.kt
@@ -5,11 +5,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "Content")
-data class ContentEntity(
-    // 서버에 가지고 온 데이터로 지속적으로 업데이트를 해주기 때문에
-    // autoGenerate = false로 설정해준다.
-    @PrimaryKey(autoGenerate = false)
+@Entity("Content")
+data class ContentEntity (
+
+    @PrimaryKey(false)
     val id: Int,
     @ColumnInfo
     var title: String,
@@ -25,4 +24,8 @@ data class ContentEntity(
     val commentCount: Int,
     @ColumnInfo
     val viewCount: Int,
-) : java.io.Serializable
+): java.io.Serializable
+
+
+// 서버에 가지고 온 데이터로 지속적으로 업데이트를 해주기 때문에
+// autoGenerate = false로 설정해준다.
---
+++ app/src/main/java/com/fast/blindappexample/domain/model/Content.kt
@@ -0,0 +1,15 @@
+package com.fast.blindappexample.domain.model
+
+import java.util.*
+
+// 클린아키텍처의 구조라서 도메인 레이어에서 데이터 레이어를 알면 안되기 때문에 도메인에도 모델을 만들어준다.
+data class Content(
+    val id: Int? = null,
+    val title: String,
+    val content: String,
+    val category: String,
+    val createdDate: Date = Date(),
+    val likeCount: Int? = 0,
+    val commentCount: Int? = 0,
+    val viewCount: Int? = 0
+) : java.io.Serializable
