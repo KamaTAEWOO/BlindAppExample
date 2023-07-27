668a6e72f3035c9913a7ed1e3d0abd9723cbbb23
zszs2020 <sakang@dk.co.kr>
2023-07-27 13:02:10 +0900

    [fix] init, scope 추가

--- app/src/main/java/com/fast/blindappexample/presenter/viewmodel/FoodViewModel.kt
+++ app/src/main/java/com/fast/blindappexample/presenter/viewmodel/FoodViewModel.kt
@@ -1,8 +1,14 @@
 package com.fast.blindappexample.presenter.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.fast.blindappexample.domain.usecase.FoodUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,7 +16,12 @@ class FoodViewModel @Inject constructor(
     private val foodUseCase: FoodUseCase
 ) : ViewModel() {
 
-    fun initLoadList() {
-        foodUseCase.loadList()
-    }
+    val foodList = foodUseCase.loadList()
+        .stateIn(
+            initialValue = emptyList(),
+            started = SharingStarted.WhileSubscribed(5000),
+            scope = viewModelScope
+        )
+
+
 }
