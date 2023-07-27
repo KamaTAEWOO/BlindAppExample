2c7b75de4574c1f2da84d338d33a4f1b0808cec9
zszs2020 <sakang@dk.co.kr>
2023-07-27 13:02:51 +0900

    [feat] observeViewModel 추가

--- app/src/main/java/com/fast/blindappexample/presenter/ui/FoodActivity.kt
+++ app/src/main/java/com/fast/blindappexample/presenter/ui/FoodActivity.kt
@@ -5,9 +5,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.fast.blindappexample.databinding.ActivityFoodBinding
 import com.fast.blindappexample.presenter.viewmodel.FoodViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class FoodActivity : AppCompatActivity() {
@@ -24,7 +30,18 @@ class FoodActivity : AppCompatActivity() {
             viewModel = this@FoodActivity.viewModel
         }
 
-        viewModel.initLoadList()
+        Timber.d("FoodActivity onCreate()")
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            viewModel.foodList
+                .flowWithLifecycle(lifecycle, Lifecycle.State.RESUMED)
+                .collectLatest {
+                    Timber.d("FoodActivity observeViewModel() collectLatest()")
+                }
+        }
     }
 
     companion object {
